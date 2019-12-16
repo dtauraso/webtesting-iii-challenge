5ae6cc718386640edd69490e6a97bb16a38cb5d2
David Tauraso <david318@me.com>
2019-12-16 13:29:45 -0800

    Got the test for unlocked working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Controls from './Controls';
+import {toggleLocked, toggleClosed } from '../dashboard/Dashboard'
+test('Unlock Gate', () => {
+
+    let locked = true
+    let closed = false
+    // let toggleLocked = true
+    // let toggleClosed = false
+    const { getByTestId, rerender } = render(<Controls 
+        locked={locked}
+        closed={closed}
+        toggleLocked={toggleLocked}
+        toggleClosed={toggleClosed}
+    />)
+
+    getByTestId('Unlock Gate 1')
+})
